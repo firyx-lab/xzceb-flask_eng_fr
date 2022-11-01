90c3ea1eebfdaf7a36277e625ff8a0f66df0d1c2
firyx-lab <filip.rysavy.nyc70@max.aleeas.com>
2022-11-01 06:09:13 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,21 @@
+import unittest
+from machinetranslation.translator import english_to_french, french_to_english
+from ibm_cloud_sdk_core.api_exception import ApiException
+
+class TestTranslator(unittest.TestCase):
+    def test_english_to_french_null(self):
+        with self.assertRaises(ApiException):
+                english_to_french('')
+    
+    def test_french_to_english_null(self):
+        with self.assertRaises(ApiException):
+                french_to_english('')
+
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+if __name__ == "__main__":
+    unittest.main()
